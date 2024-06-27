7669224f29a44f96f7a93f5568712efaa55173b7
davidcraccer <dsvoboda25@gmail.com>
2024-06-27 17:07:13 +0200

    remove table shit in habit

--- src/main/java/de/htwberlin/webtech/Habitapp/model/Habit.java
+++ src/main/java/de/htwberlin/webtech/Habitapp/model/Habit.java
@@ -4,21 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.DiscriminatorColumn;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 @Setter
 @Getter
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "habit")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Habit {
 
     @Id
