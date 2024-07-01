c30c9df4377d7e76b1337c2e04d486f32141fda3
davidcraccer <dsvoboda25@gmail.com>
2024-07-01 21:57:50 +0200

    one to many jakarta for Day

--- src/main/java/de/htwberlin/webtech/Habitapp/model/Day.java
+++ src/main/java/de/htwberlin/webtech/Habitapp/model/Day.java
@@ -1,9 +1,12 @@
 package de.htwberlin.webtech.Habitapp.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +21,7 @@ public class Day {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Habit> habits;
     private boolean completed;
 }
