02b174ea3a06349eb6b36b072dc489fe00860405
dav49261 <david.svoboda@fokus.fraunhofer.de>
2024-10-08 12:24:04 +0200

    Remove everything

--- src/main/java/de/htwberlin/webtech/Habitapp/persistence/HabitRepository.java
+++
@@ -1,8 +0,0 @@
-package de.htwberlin.webtech.Habitapp.persistence;
-
-import de.htwberlin.webtech.Habitapp.model.Habit;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HabitRepository extends CrudRepository<Habit, Long>{}
